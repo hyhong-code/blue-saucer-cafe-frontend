1193186f651042a76416f6e97e9565fedaa23c77
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-07-06 03:40:07 -0700

    Finish about section markup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// Toggle hamburger menu button icon
 $(function () {
   $(".hamburger-menu").on("click", function () {
+    // Toggle hamburger menu button icon
     $(".toggle").toggleClass("open");
     $(".nav-list").toggleClass("open");
   });
